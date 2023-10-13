7dbecabd6d878ac29da99508dea10e2ecba205a9
mrpumpkin98 <sju4486@naver.com>
2023-10-13 11:41:16 +0900

    feat : ActionSheet 컴포넌트 구현

---
+++ src/components/ActionSheetComponent.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Text, View, StyleSheet, Pressable, Image } from "react-native";
+import ActionSheet, { ActionSheetRef } from "react-native-actions-sheet";
+import generateRandomId from "../components/GenerateRandomId";
+
+const sortIcon = require("../../src/assets/sort.png");
+const longIcon = require("../../src/assets/long.png");
+const circleIcon = require("../../src/assets/circleIcon.png");
+const checkbox = require("../../src/assets/checkbox.png");
+
+export default function ActionSheetComponent({
+  actionSheetRef,
+  addAnswer,
+}: any) {
+  return (
+    <ActionSheet ref={actionSheetRef} containerStyle={styles.actionSheet}>
+      <Text>More</Text>
+      <View style={styles.select}>
+        <Pressable
+          onPress={() => addAnswer("ShortAnswer")}
+          style={styles.Pressable}
+        >
+          <Image source={sortIcon} style={styles.sortIcon} />
+          <Text>단답형</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => addAnswer("LongAnswer")}
+          style={styles.Pressable}
+        >
+          <Image source={longIcon} style={styles.longIcon} />
+          <Text>장문형</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => addAnswer("MultipleChoiceAnswer")}
+          style={styles.Pressable}
+        >
+          <Image source={circleIcon} style={styles.circleIcon} />
+          <Text>객관식</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => addAnswer("CheckBoxAnswer")}
+          style={styles.Pressable}
+        >
+          <Image source={checkbox} style={styles.checkbox} />
+          <Text>체크박스</Text>
+        </Pressable>
+      </View>
+    </ActionSheet>
+  );
+}
+
+const styles = StyleSheet.create({
+  actionSheet: {
+    height: 300,
+    padding: 30,
+  },
+  select: {
+    padding: 15,
+  },
+  Pressable: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 33,
+  },
+  checkbox: {
+    width: 22,
+    height: 22,
+    marginRight: 15,
+  },
+  sortIcon: {
+    width: 22,
+    height: 22,
+    marginRight: 15,
+  },
+  longIcon: {
+    width: 22,
+    height: 22,
+    marginRight: 15,
+  },
+  circleIcon: {
+    width: 22,
+    height: 22,
+    marginRight: 15,
+  },
+});
